745d1613cc827f6386900eef5e5ce5f17e288872
Esther Jimenez <theoriginalgameuse@outlook.fr>
2022-03-30 09:37:46 +0200

    indexer

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -39,24 +39,30 @@ exports.getOneSauce = (req, res, next) => { // création de la logique de ma rou
 exports.modifySauce = (req, res, next) => { // création de la logique de ma route put qui permet de modifier une sauces spésifique exports pour pouvoir la récuperer  et l'affect a ma route //
   Sauce.findOne({ _id: req.params.id })//nous utilisons la méthode find() dans notre modèle Mongoose afin de renvoyer un tableau contenant une  Sauce spésific de notre base de donné grace a son id contenue dans req.params//
     .then(sauce => {//ensuite une fois la sauce spésific récuper on passe un condition//
- if( req.auth.userId ===  sauce.userId){ //si l' id de l'user qui à créé la sauce et égal l'userID qui désire modifier la sauce//
-  const sauceObject = req.file ? //dans se cas là création d'un objet contenant la sauce qui sera modifier  par l'utilisateur contenue dans la requete//  
+      if( req.auth.userId ===  sauce.userId){
+        let filenam = sauce.imageUrl.split('/images/')[1];//nous créont une const qui grace split un tableaux de si qu'il y a avant l'image dans l'url et après l'image et nous récuperont le 2ème eléments du tableaux qui corespend au nom du fichier //
+        fs.unlink(`images/${filenam}`, () => { //nous utilisons ensuite la fonction unlink du package fs pour supprimer ce fichier, en lui passant le fichier à supprimer et le callback à exécuter une fois ce fichier supprimé //
+          }
+        )}
+      if( req.auth.userId ===  sauce.userId){ //si l' id de l'user qui à créé la sauce et égal l'userID qui désire modifier la sauce//
+        const sauceObject = req.file ? //dans se cas là création d'un objet contenant la sauce qui sera modifier  par l'utilisateur contenue dans la requete//
+          
+          // // opérateur ternaire pour vérifier si fichier image existe ou non S'il existe, on traite la nouvelle image ; s'il n'existe pas, on traite simplement l'objet entrant. On crée ensuite une instance Sauce à partir de sauceObject , puis on effectue la modification.//
+          {
+            ...JSON.parse(req.body.sauce), //transforme un objet stringifié en Object JavaScript exploitable.//
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`//reconstruire l'URL complète du fichier enregistré.//
+          } : { ...req.body };
+        Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id }) //nous actualisont la modification de la sauce
+          .then(() => res.status(200).json({ message: 'Objet modifié !'}))//utilisanr le paramètre id de la requête pour configurer notre Sauce avec le même _id qu'avant.//   //2ème argument le nouvelle versionde l' objet on récupere la sauce qui est dans le corps de la requete et on vérifie bien que l'id du paremettre du body est bien le meme//
+          .catch(error => res.status(400).json({ error }));
+      }
+      else
+      {
+        res.status(401).json({ error });
+      }
+    })
+};
 
-  //regarde si req.file existe ou non. S'il existe, on traite la nouvelle image ; s'il n'existe pas, on traite simplement l'objet entrant. On crée ensuite une instance Sauce à partir de sauceObject , puis on effectue la modification.//                                
-    {
-      ...JSON.parse(req.body.sauce), //transforme un objet stringifié en Object JavaScript exploitable.//
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`//reconstruire l'URL complète du fichier enregistré.//
-    } : { ...req.body };
-  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id }) //nous actualisont la modification de la sauce
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))//utilisanr le paramètre id de la requête pour configurer notre Sauce avec le même _id qu'avant.//   //2ème argument le nouvelle versionde l' objet on récupere la sauce qui est dans le corps de la requete et on vérifie bien que l'id du paremettre du body est bien le meme//
-    .catch(error => res.status(400).json({ error }));
-  } 
-  else
-  {
-  res.status(401).json({ error });
-  }
-})
-}; 
 
 // création de la logique de ma route delete qui permet de suprimmer  une sauces spésifique exports pour pouvoir la récuperer  et l'affecté a ma route //
 exports.deleteSauce = (req, res, next) => {
